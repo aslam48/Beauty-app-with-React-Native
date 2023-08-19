59cbd714ad61fad551c2c06fcf4c76ff99fd22d6
aslam48 <youngfizzy001@gmail.com>
2023-08-19 02:38:38 +0100

    working

---
+++ sever/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'hqiw07yb',
+    dataset: 'production'
+  }
+})
---
+++ sever/schemas/index.js
@@ -0,0 +1,7 @@
+import category from './category';
+import product from './product';
+
+export const schemaTypes = [product, category]
+
+
+// https://react-native-beauty-app.sanity.studio/
