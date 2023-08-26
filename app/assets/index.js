cf24192ca34cf0c8214eaf5efc6086c13a17a6df
aslam48 <youngfizzy001@gmail.com>
2023-08-26 18:24:51 +0100

    working

--- app/assets/index.js
+++ app/assets/index.js
@@ -1,5 +1,5 @@
 export {default as Brand} from "./brand.png"
 export {default as Screen1} from "./screen1.png"
-export {default as Screen2} from "./screen2.png"
-export {default as Screen3} from "./screen3.png"
+export {default as Screen2} from "./screen2.jpg"
+export {default as Screen3} from "./screen3.jpeg"
 export {default as EmptyCart} from "./emptycart.png"
--- app/screens/HomeScreen.jsx
+++ app/screens/HomeScreen.jsx
@@ -1,15 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import {FontAwesome, MaterialIcons } from '@expo/vector-icons';
+import { Screen3 } from '../assets';
 
 const HomeScreen = () => {
   return (
-    <View className="flex-1 justify-center align-center">
-    <Text className="text-red-500">Home Screen</Text>
-    <StatusBar style="auto" />
+    <SafeAreaView className="flex-1 justify-start align-center bg-[#EBEAEF]">
+  <View className="w-full flex-row items-center justify-between px-4 py-2">
+   <MaterialIcons name="chevron-left" size={32} color="#555" />
+   <Image source={Screen3} resizeMode='cover' className="w-12 h-12 rounded-xl"/>
   </View>
+
+
+  {/* searc box  */}
+  <View className="flex-row items-center justify-between px-4 py-2 w-full space-x-6">
+        <View className="px-4 py-2 bg-white rounded-xl flex-1 flex-row items-center justify-center space-x-2">
+          <MaterialIcons name="search" size={24} color="#7f7f7f" />
+          <TextInput
+            className="text-base font-semibold text=[#555] flex-1 py-1 -mt-1 "
+            placeholder="Search Here..."
+            // value={searchTerm}
+            // onChangeText={handleSearchTerm}
+          />
+        </View>
+
+        <TouchableOpacity className="w-12 h-12 rounded-xl flex items-center justify-center bg-white">
+          <FontAwesome name="filter" size={24} color="#7f7f7f" />
+        </TouchableOpacity>
+      </View>
+  </SafeAreaView>
   )
 }
 
 export default HomeScreen
-
-const styles = StyleSheet.create({})
